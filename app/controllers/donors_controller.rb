401f90f9de47fb1c53b9fb55910c4e415403d190
8ceg6 <8ceg6@comcast.net>
2020-06-05 07:03:16 -0400

    rails s not working

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -1,2 +1,17 @@
 class DonorsController < ApplicationController
+
+    def new 
+        @donor = Donor.all 
+    end 
+
+    def def create 
+        @donor = Donor.create(donor_params)
+    end 
+
+
+    private 
+
+    def donor_params 
+        params.require(:donor).permit(:name, :email, :password)
+    end 
 end
