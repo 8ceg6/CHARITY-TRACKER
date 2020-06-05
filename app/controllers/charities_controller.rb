4407469746e69a876011f93817717adf0666026f
8ceg6 <8ceg6@comcast.net>
2020-06-04 21:14:58 -0400

    moved project to directory so i can use rails s command

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -1,2 +1,24 @@
 class CharitiesController < ApplicationController
+
+    def new 
+        @charity = Charity.new 
+    end 
+
+    def create 
+        @charity = Charity.create(charity_params)
+        redirect_to charity_path(@charity)
+    end 
+
+
+
+
+
+private 
+
+    def charity_params 
+        params.require(:charity).permit(:name, :email,)
+    end 
+
 end
+
+
