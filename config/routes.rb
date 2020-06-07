cd82464cb04a64db02e4be062b563b545c28a026
8ceg6 <8ceg6@comcast.net>
2020-06-07 01:26:47 -0400

    set up session index page for logging in

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   resources :donors do 
     resources :donations, only: [:new, :index]
   end 
+  get '/' => 'session#index'
+  get '/signin' => 'session#new'
+  post '/signin' => 'session#create'  
+  get '/logout' => 'session#destroy'
+  get '/session/new'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
